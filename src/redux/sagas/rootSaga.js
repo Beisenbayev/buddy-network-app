f0bb37917e548ca6a5bd067ec66faf046feff840
beisenbayev <beisenbayeff.nursultan@mail.ru>
2021-09-23 12:04:31 +0600

    members-page [refactoring (redux-saga)]

--- src/redux/sagas/rootSaga.js
+++ src/redux/sagas/rootSaga.js
@@ -9,6 +9,14 @@ import {
    GET_NEW_MESSAGES_COUNT, handleGetNewMessagesCount
 } from '../reducers/messages-reducer.js';
 
+import {
+   GET_MEMBERS, handleGetMembers,
+   GET_FRIENDS, handleGetFriends,
+   GET_NEWCOMERS, handleGetNewcomers,
+   FOLLOW, handleFollow,
+   UNFOLLOW, handleUnfollow
+} from '../reducers/members-reducer';
+
 function* watcherSaga() {
    yield takeEvery(START_NEW_CHAT, handleStartNewChat);
    yield takeEvery(GET_DIALOGS, handleGetDialogs);
@@ -16,6 +24,12 @@ function* watcherSaga() {
    yield takeEvery(SEND_NEW_MESSAGE, handleSendNewMessage);
    yield takeEvery(GET_LAST_MESSAGE_STATE, handleGetLastMessageState);
    yield takeEvery(GET_NEW_MESSAGES_COUNT, handleGetNewMessagesCount);
+
+   yield takeEvery(GET_MEMBERS, handleGetMembers);
+   yield takeEvery(GET_FRIENDS, handleGetFriends);
+   yield takeEvery(GET_NEWCOMERS, handleGetNewcomers);
+   yield takeEvery(FOLLOW, handleFollow);
+   yield takeEvery(UNFOLLOW, handleUnfollow);
 }
 
 
